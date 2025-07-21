5f717dba90d1581dc8dd6cbd188af8ec45a91cb3
Rafael Batista <rafael@devbatista.com>
2025-07-20 23:05:18 -0300

    users_controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+  load_and_authorize_resource
+  
+  def index
+    @users = case current_user.role
+            when 'admin'
+              @users.order(:name)
+            when 'gestor'
+              @users.tecnicos.order(:name)
+            end
+  end
+
+  def show
+    @assignments = @user.assignments.includes(:order_service)
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Usuário atualizado com sucesso.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    permitted = [:name]
+    permitted << :role if can?(:manage, User)
+    params.require(:user).permit(permitted)
+  end
 end
