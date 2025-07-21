ff99dd94939648bb9213696bfad7bcc0d7557331
Rafael Batista <rafael@devbatista.com>
2025-07-21 16:58:27 -0300

    colocando uuid como padrão antes de iniciar o objeto

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  after_initialize :ensure_uuid_for_primary_key
+
+  private
+
+  def ensure_uuid_for_primary_key
+    if self.class.primary_key == "id" && self.id.blank? && self.class.columns_hash["id"]&.type == :uuid
+      self.id = SecureRandom.uuid
+    end
+  end
 end
