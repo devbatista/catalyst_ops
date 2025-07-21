954e498cb05b535c6b0d23d27269bc90e7efb1f7
Rafael Batista <rafael@devbatista.com>
2025-07-20 22:52:01 -0300

    clients_controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,47 @@
 class ClientsController < ApplicationController
+  load_and_authorize_resource
+  
+  def index
+    @clients = @clients.includes(:order_services)
+                       .order(:name)
+                       .page(params[:page])
+  end
+
+  def show
+    @order_services = @client.order_services
+                             .includes(:users)
+                             .order(created_at: :desc)
+  end
+
+  def new;end
+
+  def create
+    if @client.save
+      redirect_to @client, notice: 'Cliente criado com sucesso.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @client.update(client_params)
+      redirect_to @client, notice: 'Cliente atualizado com sucesso.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @client.destroy
+    redirect_to clients_path, notice: 'Cliente removido com sucesso.'
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name, :document, :email, :phone, :address)
+  end
 end
