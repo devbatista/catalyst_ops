5aaf22d4cbb6a4789f5d064385340751ebb7bbd9
Rafael Batista <rafael@devbatista.com>
2025-07-20 21:03:17 -0300

    configurando cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,32 +1,77 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+    user ||= User.new
+
+    case user.role
+    when 'admin'
+      admin_abilities
+    when 'gestor'
+      gestor_abilities(user)
+    when 'tecnico'
+      tecnico_abilities(user)
+    else
+      guest_abilities
+    end
+  end
+
+  private
+
+  def admin_abilities
+    # Admin pode tudo
+    can :manage, :all
+  end
+
+  def gestor_abilities(user)
+    # Pode gerenciar clientes
+    can :manage, Client
+    
+    # Pode gerenciar ordens de serviço
+    can :manage, OrderService
+    
+    # Pode gerenciar atribuições
+    can :manage, Assignment
+    
+    # Pode gerenciar itens de serviço
+    can :manage, ServiceItem
+    
+    # Pode visualizar técnicos
+    can :read, User, role: 'tecnico'
+    
+    # Pode editar próprio perfil
+    can [:read, :update], User, id: user.id
+  end
+
+  def tecnico_abilities(user)
+    # Pode visualizar apenas OSs atribuídas a ele
+    can :read, OrderService do |order_service|
+      order_service.users.include?(user)
+    end
+    
+    # Pode atualizar status das OSs atribuídas
+    can :update, OrderService do |order_service|
+      order_service.users.include?(user) && !order_service.concluida?
+    end
+    
+    # Pode gerenciar itens de serviço das suas OSs
+    can :manage, ServiceItem do |service_item|
+      service_item.order_service.users.include?(user)
+    end
+    
+    # Pode ver próprias atribuições
+    can :read, Assignment, user_id: user.id
+    
+    # Pode editar próprio perfil
+    can [:read, :update], User, id: user.id
+    
+    # Pode visualizar clientes das suas OSs
+    can :read, Client do |client|
+      client.order_services.joins(:users).where(users: { id: user.id }).any?
+    end
+  end
+
+  def guest_abilities
+    # Usuários não logados não podem fazer nada
   end
 end
