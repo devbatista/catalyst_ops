a7aa1764e31c1b0ace051b16b126597347c331cf
Rafael Batista <rafael@devbatista.com>
2025-07-31 18:01:06 -0300

    ajustando rotas e dominio para reconfiguração de senha

--- config/routes/login.rb
+++ config/routes/login.rb
@@ -3,5 +3,7 @@ constraints subdomain: "login" do
     get "/", to: "sessions#new", as: :login_root
     post "/login", to: "sessions#create", as: :login
     delete "/logout", to: "sessions#destroy", as: :logout
+    
+    get "/new_password", to: "sessions#new_password", as: :new_password
   end
 end
