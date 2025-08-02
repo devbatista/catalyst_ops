7c9b9a9e17e4e88f1ac7ade775433d93d21fd024
Rafael Batista <rafael@devbatista.com>
2025-08-02 10:11:49 -0300

    colocando o valor dos itens no dashboard

--- app/controllers/app/dashboard_controller.rb
+++ app/controllers/app/dashboard_controller.rb
@@ -11,14 +11,9 @@ class App::DashboardController < ApplicationController
       @recent_orders = OrderService.includes(:client).order(created_at: :desc).limit(5)
     when 'gestor'
       @clients_count = current_user.clients.count
-      @order_services_count = current_user.company.order_services.count
+      @order_services = current_user.company.order_services
       @technicians_count = User.where(role: :tecnico, company_id: current_user.company_id).count
-      @pending_orders = OrderService.agendada.count
-      @in_progress_orders = OrderService.em_andamento.count
-      @recent_orders = current_user.company.order_services
-                                           .includes(:client)
-                                           .order(created_at: :desc)
-                                           .limit(6)
+      @recent_orders = @order_services.order(created_at: :desc).limit(6)
     when 'tecnico'
       @my_orders = current_user.order_services.includes(:client)
       @pending_orders = @my_orders.agendada.count
