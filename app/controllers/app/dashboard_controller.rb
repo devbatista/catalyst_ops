6386c0b209a0ff9fcf220bb364334f6901e07c38
Rafael Batista <rafael@devbatista.com>
2025-08-02 16:42:02 -0300

    ajuste de dashboard para técnicos

--- app/controllers/app/dashboard_controller.rb
+++ app/controllers/app/dashboard_controller.rb
@@ -1,20 +1,22 @@
 class App::DashboardController < ApplicationController
   def index
     authorize! :read, :dashboard
-    
+
     case current_user.role
-    when 'admin'
+    when "admin"
       @clients_count = Client.count
       @order_services_count = OrderService.count
       @pending_orders = OrderService.agendada.count
       @in_progress_orders = OrderService.em_andamento.count
       @recent_orders = OrderService.includes(:client).order(created_at: :desc).limit(5)
-    when 'gestor'
+    when "gestor"
       @clients_count = current_user.clients.count
       @order_services = current_user.company.order_services
       @technicians_count = User.where(role: :tecnico, company_id: current_user.company_id).count
       @recent_orders = @order_services.order(created_at: :desc).limit(6)
-    when 'tecnico'
+    when "tecnico"
+      @order_services = current_user.company.order_services
+      @recent_orders = @order_services.order(created_at: :desc).limit(6)
       @my_orders = current_user.order_services.includes(:client)
       @pending_orders = @my_orders.agendada.count
       @in_progress_orders = @my_orders.em_andamento.count
