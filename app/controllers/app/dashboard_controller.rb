c43cd81bb73c55205834369c049954305c9481d5
Rafael Batista <rafael@devbatista.com>
2025-08-02 00:41:52 -0300

    ajustando dashboard

--- app/controllers/app/dashboard_controller.rb
+++ app/controllers/app/dashboard_controller.rb
@@ -15,7 +15,10 @@ class App::DashboardController < ApplicationController
       @technicians_count = User.where(role: :tecnico, company_id: current_user.company_id).count
       @pending_orders = OrderService.agendada.count
       @in_progress_orders = OrderService.em_andamento.count
-      @recent_orders = OrderService.includes(:client).order(created_at: :desc).limit(5)
+      @recent_orders = current_user.company.order_services
+                                           .includes(:client)
+                                           .order(created_at: :desc)
+                                           .limit(6)
     when 'tecnico'
       @my_orders = current_user.order_services.includes(:client)
       @pending_orders = @my_orders.agendada.count
