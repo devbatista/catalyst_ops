a2f2e5490f78ec689b52d3db7d5c80a7036cd256
Rafael Batista <rafael@devbatista.com>
2025-08-04 09:34:47 -0300

    criando método pra converter valores

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def active_menu_class(path)
     current_page?(path) ? "active" : ""
   end
+
+  def currency_br(value)
+    number_to_currency(
+      value,
+      unit: "R$ ",
+      separator: ",",
+      delimiter: "."
+    )
+  end
 end
