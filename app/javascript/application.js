2899817bb631e94ea96026d2b37cadac28a7ace7
Rafael Batista <rafael@devbatista.com>
2025-08-12 17:14:44 -0300

    corrigindo erro de cors e de utilização do turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers"
-import * as Rails from "@rails/ujs"
-Rails.start()
 
 document.addEventListener("turbo:load", () => {
   const psEl = document.querySelector(".js-perfect-scrollbar")
