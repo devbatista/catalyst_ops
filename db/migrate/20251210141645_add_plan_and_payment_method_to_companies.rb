dcd666546810b8596540f9e9853ea1428e5578f9
Rafael Batista <rafael@devbatista.com>
2025-12-11 06:43:18 -0300

    migration

--- db/migrate/20251210141645_add_plan_and_payment_method_to_companies.rb
+++ db/migrate/20251210141645_add_plan_and_payment_method_to_companies.rb
@@ -1,6 +1,6 @@
 class AddPlanAndPaymentMethodToCompanies < ActiveRecord::Migration[7.1]
   def change
-    add_reference :companies, :plan, foreign_key: true, null: false
+    add_reference :companies, :plan, type: :uuid, null: true, foreign_key: true
     add_column :companies, :payment_method, :string, null: false, default: "boleto"
 
     add_index :companies, :payment_method
