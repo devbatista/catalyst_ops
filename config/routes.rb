0cd992ba9992b5c0149db2824fbb1d755636231c
Rafael Batista <rafael@devbatista.com>
2026-01-04 14:02:49 -0300

    ajustando as rotas como eram anteriormente

--- config/routes.rb
+++ config/routes.rb
@@ -3,37 +3,18 @@ def draw(routes_name)
 end
 
 Rails.application.routes.draw do
-  # --- CATCH-ALL PARA DEBUG (primeiro para garantir que pega antes dos constraints) ---
-  match "/", to: proc { |env|
-    [
-      200,
-      { "Content-Type" => "text/plain" },
-      ["DEBUG: HOST=#{env['HTTP_HOST']} | PATH=#{env['PATH_INFO']}"]
-    ]
-  }, via: :all
+  devise_for :users, skip: [:sessions]
 
-  # devise_for :users, skip: [:sessions]
-
-  # draw :admin
-  # draw :app
-  # draw :login
-  # draw :register
-  # draw :sidekiq
+  draw :admin
+  draw :app
+  draw :login
+  draw :register
 
   # Rotas padrão (sem subdomínio)
   # root to: "home#index"
-  # match '/', to: proc { [204, {}, []] }, via: :options
+  match '/', to: proc { [204, {}, []] }, via: :options
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-
-  # --- CATCH-ALL PARA OUTROS PATHS ---
-  match "*path", to: proc { |env|
-    [
-      200,
-      { "Content-Type" => "text/plain" },
-      ["DEBUG: HOST=#{env['HTTP_HOST']} | PATH=#{env['PATH_INFO']}"]
-    ]
-  }, via: :all
 end
