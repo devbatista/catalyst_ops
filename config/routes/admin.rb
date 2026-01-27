2a990e252b86ed413ea275bf380e60c7583723c9
Rafael Batista <rafael@devbatista.com>
2026-01-27 11:04:00 -0300

    ajuste de rotas

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -1,10 +1,15 @@
 constraints subdomain: "admin" do
   get "/", to: "admin/dashboard#index", as: :admin_dashboard
 
-  namespace :admin do
-    resources :companies
-  end
-
+  resources :companies, module: "admin", as: :admin_companies
+  resources :users, module: "admin", as: :admin_users
+  resources :subscriptions, module: "admin", as: :admin_subscriptions
+  resources :plans, module: "admin", as: :admin_plans
+  resources :order_services, module: "admin", as: :admin_order_services
+  resources :support, only: [:index], module: "admin", as: :admin_support
+  resources :configurations, only: [:index, :edit, :update], module: "admin", as: :admin_configurations
+  resources :contents, module: "admin", as: :admin_contents
+  
   devise_scope :user do
     delete "/logout", to: "sessions#destroy", as: :logout_admin
   end
