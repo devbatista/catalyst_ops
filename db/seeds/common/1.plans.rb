75201cb1e300eea8e7b840e172a4acb84f79dd13
Rafael Batista <rafael@devbatista.com>
2026-03-17 17:55:44 -0300

    ajustando o seed de plans

--- db/seeds/common/1.plans.rb
+++ db/seeds/common/1.plans.rb
@@ -4,7 +4,7 @@ Plan.find_or_create_by!(external_reference: "BASICO") do |plan|
   plan.name = "Basico"
   plan.reason = "c-basico"
   plan.status = "active"
-  plan.external_id = "0d5ca5de0e754022b41bcb59408924c1"
+  plan.external_id = "e38f93dc444940c7b0f326c734752883"
   plan.frequency = 1
   plan.frequency_type = "months"
   plan.transaction_amount = 99.0
@@ -17,7 +17,7 @@ Plan.find_or_create_by!(external_reference: "PROFISSIONAL") do |plan|
   plan.name = "Profissional"
   plan.reason = "c-profissional"
   plan.status = "active"
-  plan.external_id = "2afd21a0af3448a8b1b56f7ef2f96ca8"
+  plan.external_id = "0dd58b3ca8b349b892ec9bdc151c9747"
   plan.frequency = 1
   plan.frequency_type = "months"
   plan.transaction_amount = 199.0
@@ -30,7 +30,7 @@ Plan.find_or_create_by!(external_reference: "ENTERPRISE") do |plan|
   plan.name = "Enterprise"
   plan.reason = "c-enterprise"
   plan.status = "active"
-  plan.external_id = "b09c62e9e6954d0eb02a83110df03520"
+  plan.external_id = "42eee0e64dfb4ff5a52e710da6e86fdc"
   plan.frequency = 1
   plan.frequency_type = "months"
   plan.transaction_amount = 399.0
