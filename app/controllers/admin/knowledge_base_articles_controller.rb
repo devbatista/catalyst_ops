69e9b22a4a576e76fa0618af622185dfa0e88773
Rafael Batista <rafael@devbatista.com>
2026-03-18 19:56:16 -0300

    ajuste do form

---
+++ app/controllers/admin/knowledge_base_articles_controller.rb
@@ -0,0 +1,66 @@
+class Admin::KnowledgeBaseArticlesController < AdminController
+  before_action :set_knowledge_base_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @all_categories = KnowledgeBaseArticle.distinct.order(:category).pluck(:category).compact
+
+    @articles = KnowledgeBaseArticle.order(:category, :title)
+    if params[:category].present?
+      @articles = @articles.where(category: params[:category])
+    end
+
+    if params[:q].present?
+      q = "%#{params[:q]}%"
+      @articles = @articles.where("title ILIKE ? OR content ILIKE ?", q, q)
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @all_categories = KnowledgeBaseArticle.distinct.order(:category).pluck(:category).compact
+  end
+
+  def edit
+    @all_categories = KnowledgeBaseArticle.distinct.order(:category).pluck(:category).compact
+  end
+
+  def create
+    @knowledge_base_article = KnowledgeBaseArticle.new(knowledge_base_article_params)
+
+    if @knowledge_base_article.save
+      redirect_to admin_knowledge_base_index_path, notice: "Artigo criado com sucesso."
+    else
+      @all_categories = KnowledgeBaseArticle.distinct.order(:category).pluck(:category).compact
+      render :new
+    end
+  end
+
+  def update
+    if @knowledge_base_article.update(knowledge_base_article_params)
+      redirect_to admin_knowledge_base_path(@knowledge_base_article), notice: "Artigo atualizado com sucesso."
+    else
+      @all_categories = KnowledgeBaseArticle.distinct.order(:category).pluck(:category).compact
+      render :edit
+    end
+  end
+
+  def destroy
+    if @knowledge_base_article.destroy
+      redirect_to admin_knowledge_base_index_path, notice: "Artigo excluído com sucesso."
+    else
+      redirect_to admin_knowledge_base_index_path, alert: "Não foi possível excluir o artigo."
+    end
+  end
+  
+  private
+
+  def set_knowledge_base_article
+    @knowledge_base_article = KnowledgeBaseArticle.find(params[:id])
+  end
+
+  def knowledge_base_article_params
+    params.require(:knowledge_base_article).permit(:title, :content, :category)
+  end
+end
--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -16,7 +16,7 @@ constraints subdomain: "admin" do
     end
   end
   resources :support_messages, only: [:create], module: "admin", as: :admin_support_messages
-  resources :knowledge_base, module: "admin", as: :admin_knowledge_base
+  resources :knowledge_base_articles, module: "admin", as: :admin_knowledge_base
   resources :configurations, only: [:index, :edit, :update], module: "admin", as: :admin_configurations
   resources :contents, module: "admin", as: :admin_contents
   
