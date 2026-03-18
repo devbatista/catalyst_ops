029e4fd32e7fb0e2572967ae14dc73f05d71590b
Rafael Batista <rafael@devbatista.com>
2026-03-18 18:21:15 -0300

    colocando a tela pra criar os artigos

--- app/controllers/admin/knowledge_base_controller.rb
+++ app/controllers/admin/knowledge_base_controller.rb
@@ -1,5 +1,29 @@
 class Admin::KnowledgeBaseController < AdminController
   def index
+    @all_categories = KnowledgeBaseArticle.distinct.order(:category).pluck(:category).compact
+
     @articles = KnowledgeBaseArticle.order(:category, :title)
+    if params[:category].present?
+      @articles = @articles.where(category: params[:category])
+    end
+
+    if params[:q].present?
+      q = "%#{params[:q]}%"
+      @articles = @articles.where("title ILIKE ? OR content ILIKE ?", q, q)
+    end
+  end
+
+  def show
+    @article = KnowledgeBaseArticle.find(params[:id])
+  end
+
+  def new
+    @knowledge_base_article = KnowledgeBaseArticle.new
+    @all_categories = KnowledgeBaseArticle.distinct.order(:category).pluck(:category).compact
+  end
+
+  def edit
+    @knowledge_base_article = KnowledgeBaseArticle.find(params[:id])
+    @all_categories = KnowledgeBaseArticle.distinct.order(:category).pluck(:category).compact
   end
 end
