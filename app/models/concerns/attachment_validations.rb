5838d10b297a6640efce488104b6fb326188b61a
Rafael Batista <rafael@devbatista.com>
2026-03-18 10:39:23 -0300

    dimunuindo limite de anexo

--- app/models/concerns/attachment_validations.rb
+++ app/models/concerns/attachment_validations.rb
@@ -10,7 +10,7 @@ module AttachmentValidations
         'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
         'video/mp4', 'video/quicktime', 'video/x-msvideo', 'video/x-ms-wmv'
       ],
-      size: { less_than: 20.megabytes, message: 'deve ter menos de 20MB' },
+      size: { less_than: 10.megabytes, message: 'deve ter menos de 10MB' },
       limit: { max: 5, message: 'máximo de 5 arquivos por registro' }
   end
 end
