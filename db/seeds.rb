26a11af718596fa4ad4483e2082b681fc5120874
Rafael Batista <rafael@devbatista.com>
2026-04-08 17:08:51 -0300

    ajuste nos seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+if Rails.env.production?
+  forced_by_flag = ARGV.include?("--force")
+  forced_by_env = ENV["SEED_FORCE"] == "1"
+
+  unless forced_by_flag || forced_by_env
+    abort <<~MSG
+      [db:seed] Execucao bloqueada em producao.
+      Operacao requer confirmacao explicita.
+    MSG
+  end
+end
+
 Dir[Rails.root.join('db/seeds/common/*.rb')].sort.each { |file| load file }
 
 env_dir = Rails.root.join('db', 'seeds', Rails.env)
